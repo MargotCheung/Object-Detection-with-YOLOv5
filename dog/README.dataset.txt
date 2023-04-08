86cf4d953d75c7ed156b36c3ab8da93e210abdc1
mch <margot.cheung@gmail.com>
2023-04-09 02:39:58 +0800

    initial commit

---
+++ dog/README.dataset.txt
@@ -0,0 +1,6 @@
+# dogs > 2023-02-20 12:35pm
+https://universe.roboflow.com/marisabel/dogs-2alwb
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ test.py
@@ -0,0 +1,17 @@
+import torch
+
+# Model
+model = torch.hub.load("ultralytics/yolov5", "yolov5s")  # or yolov5n - yolov5x6, custom
+
+# Images
+img = "dog.jpg"  # or file, Path, PIL, OpenCV, numpy, list
+
+# Inference
+results = model(img)
+
+# Results
+results.print()  # or .show(), .save(), .crop(), .pandas(), etc.
+
+results.show()
+
+results.save()
